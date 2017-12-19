920eb457cd51ccb95272bf144a352a5676e2c260
Eden Mesfun <emesfun@gmail.com>
2017-12-19 16:26:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var chocolateBars = [
   `skittles`];
 
 function addElementToBeginningOfArray(array, element){
-    chocolateBars.unshift(`foo`, 1);
+  chocolateBars.unshift(`foo`, 1);
   chocolateBars.push(`foo`, 1);
   array(n).to.deeply.equal((`foo`,1));
   
