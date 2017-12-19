5d836c742e684f1a13c746b2ae88a0a0dfb52904
Eden Mesfun <emesfun@gmail.com>
2017-12-19 15:59:38 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = [
+  `snickers`,
+  `hundred grand`,
+  `kitkat`,
+  `skittles`,];
+
+
+function addElementToBeginningOfArray(array, element){
+  chocolateBars.unshift(`foo`, 1);
+  chocolateBars.push(`foo`, 1);
+  //if (addElementToBeginningOfArray([1],`foo`) === [`foo`, 1]);
+  return chocolateBars;
+}
+
+
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  chocolateBars.unshift(`hershey`);
+  chocolateBars.push(`toblerone`);
+  if (1 === `foo`, 1);
+  return chocolateBars;
+
+  // chocolateBars[1] = `foo`;
+  
+}
+
+function addElementToEndOfArray(array, element){}
+
+function destructivelyAddElementToEndOfArray(array, element){}
+
